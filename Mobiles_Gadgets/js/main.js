b48626457059928305c7f737821989e0df3630b0
MajiroIkione <Adexandr.karpushov@ya.ru>
2025-03-13 14:46:58 +0300

    Delete Mobiles_Gadgets directory

--- Mobiles_Gadgets/js/main.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector('.burger__btn');
-
-burger.addEventListener('click', () =>{
-    burger.classList.toggle('burger__btn--active')
-    document.querySelector('.nav__list').classList.toggle('open')
-})
