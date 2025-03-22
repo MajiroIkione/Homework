65d3a4414edc2f6ed6c3a0e4cf6236129d0a3da1
MajiroIkione <Adexandr.karpushov@ya.ru>
2025-03-22 14:52:23 +0300

    Add files via upload

---
+++ PHP_WordPress/Lesson-11/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Lessons</title>
+</head>
+<body>
+
+<br>
+<br>
+<div>
+
+<?php
+
+include'counter-even.php';
+
+include'output-cycle.php'
+?>
+
+
+
+</div>
+</body>
+</html>
---
+++ PHP_WordPress/Lesson-11/output.php
@@ -0,0 +1,10 @@
+    <?php
+
+    function Text($tag, $desc){
+
+    echo"<$tag> $desc </$tag >" . "<br>";
+    
+    }
+
+    Text('h1', 'another title')
+        ?>
