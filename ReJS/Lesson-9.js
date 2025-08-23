857fd214ef2c1f009fd8dde9eb2392a70c9a1f3a
MajiroIkione <Adexandr.karpushov@ya.ru>
2025-08-23 13:42:32 +0300

    Add files via upload

---
+++ ReJS/Lesson-9.js
@@ -0,0 +1,26 @@
+// 1
+const firstName = "Matew";
+const lastName = "Brisk";
+const isStudent = true;
+
+// 2
+const age = 21;
+let currentYear = 2023;
+const birthYear = currentYear - age;
+
+// 3
+console.log(`My name is ${firstName} ${lastName}, i'm ${age} years old.  Am i a student? ${isStudent}.`);
+
+//4
+//1234567890false
+
+let a ='123';
+let b = +'456'
+let c = Number('789');
+let d = Boolean('0');
+let e = Boolean('');
+
+
+let result = a+b+c+d+e;
+console.log(typeof result);
+console.log('It was obvious after looking on the first and the result strokes');
